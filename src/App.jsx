7fd997d249b2d96679518ac888e121d9f4b2f3bf
tack-k <tyool9c@gmail.com>
2020-11-22 09:11:42 +0900

    基本形の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
 
+export default class App extends React.Ccompornent {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+render() {
+  return (
+    
+  )
+}
+
+  
+}

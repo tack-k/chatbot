34ce3c05cbc3a491ef47dc063c7931e0c5d8922b
tack-k <tyool9c@gmail.com>
2020-11-22 10:48:18 +0900

    マテリアルUI,AnswerListからデータ取得

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const Answer = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
 
+  },
+}));
+
+const Answer = (props) => {
+  // const classes = useStyles();
   return (
-    <div className=""></div>
+    <Button variant="contained" color="primary">
+      {props.content}
+    </Button>
   )
 }
 

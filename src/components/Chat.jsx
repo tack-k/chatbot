dbe15390ef59e0cb51376f75ccfb78e8f576a283
tack-k <tyool9c@gmail.com>
2020-11-22 11:24:06 +0900

    マテリアルUIリスト導入

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 const Chat = () => {
 
   return (
-<div className=""></div>
+    <ListItem>
+    <ListItemAvatar>
+   
+        <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+
+    </ListItemAvatar>
+    <div className="p-chat__bubble">
+
+    </div>
+  </ListItem>
   )
 }
 
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import {Chat} from './index';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
 
-const Chats = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+
+}));
+
+const Chats = (props) => {
+
+  const classes = useStyles();
 
   return (
-<div className=""></div>
+    <List className={classes.root}>
+
+    </List>
   )
 }
 

078a6155c4a0e4a5564c39b350c2ad169ee521a1
tack-k <tyool9c@gmail.com>
2020-11-22 15:31:42 +0900

    回答のスタイル変更

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, withTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { green } from '@material-ui/core/colors';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-
-  },
-}));
+const useStyles = makeStyles(() => (
+  createStyles({
+    "button": {
+      borderColor: '#003399',
+      color: '#003399',
+      fontWeight: 700,
+      marginBottom: '8px',
+      "&:hover": {
+        backgroundColor: '#003399',
+        color: '#FFFFFF'
+      }
+    }
+  })
+));
 
 const Answer = (props) => {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Button variant="contained" color="primary" onClick={() => {props.select(props.content, props.nextId)}}>
+    <Button variant="outlined" className={classes.button} onClick={() => {props.select(props.content, props.nextId)}}>
       {props.content}
     </Button>
   )

51f768b02de82970a30f11a522ea72e88fa29179
tack-k <tyool9c@gmail.com>
2020-11-22 11:38:08 +0900

    質問・回答による場合分け

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,17 +4,23 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 
-const Chat = () => {
+const Chat = (props) => {
+
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
 
   return (
-    <ListItem>
+    <ListItem className={classes}>
     <ListItemAvatar>
-   
+      {isQuestion ? (
         <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+        ) : (
+          <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+      )}
 
     </ListItemAvatar>
     <div className="p-chat__bubble">
-
+          {props.text}
     </div>
   </ListItem>
   )

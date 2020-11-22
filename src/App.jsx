08ce1fb2992ad9d81f09f2f085a475e23914a4a7
tack-k <tyool9c@gmail.com>
2020-11-22 09:16:33 +0900

    stateの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import defaultDataset from './dataset';
 
 export default class App extends React.Ccompornent {
   constructor(props) {
     super(props);
     this.state = {
-
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false
     }
   }
 
 render() {
   return (
-    
+
   )
 }
 

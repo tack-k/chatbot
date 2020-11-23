3402f5ec9e474a7a5eefe7d8dd9f135c235cf0b9
tack-k <tyool9c@gmail.com>
2020-11-23 16:45:11 +0900

    入力フォームのバリデーション設定

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -8,105 +8,137 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import TextInput from './TextInput';
 
 export default class FormDialog extends React.Component {
-constructor(props){
-  super(props);
-  this.state={
-    name: "",
-    email: "",
-    description: ""
-  }
-
-  this.inputName = this.inputName.bind(this)
-  this.inputEmail = this.inputEmail.bind(this)
-  this.inputDescription = this.inputDescription.bind(this)
-
-}
-
-inputName = (event) => {
-  this.setState({
-    name: event.target.value
-  })
-}
-
-inputEmail = (event) => {
-  this.setState({
-    email: event.target.value
-  })
-}
-
-inputDescription = (event) => {
-  this.setState({
-    description: event.target.value
-  })
-}
-
-submitForm = () => {
-  const name = this.state.name
-  const email = this.state.email
-  const description = this.state.description
-
-  const payload = {
-    text: '■お問い合わせがありました\n' +
-          'お名前:' + name + '\n' +
-          'Email:' + email + '\n' +
-          'お問い合わせ内容:' + '\n' + description
-  }
-
-  const url = 'https://hooks.slack.com/services/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'
-
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(payload)
-  }).then(() => {
-    alert('送信が完了しました。追ってご連絡します!')
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
       name: "",
       email: "",
       description: ""
+    }
+
+    this.inputName = this.inputName.bind(this)
+    this.inputEmail = this.inputEmail.bind(this)
+    this.inputDescription = this.inputDescription.bind(this)
+
+  }
+
+  inputName = (event) => {
+    this.setState({
+      name: event.target.value
     })
-    return this.props.handleClose()
-  })
+  }
+
+  inputEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  inputDescription = (event) => {
+    this.setState({
+      description: event.target.value
+    })
+  }
+
+  validateRequiredInput = (...args) => {
+    let isBlank = false;
+    for (let i = 0; i < args.length; i = (i + 1) | 0) {
+      if (args[i] === "") {
+        isBlank = true
+      }
+    }
+    return isBlank
+  }
+
+  validateEmailFormat = (email) => {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    return regex.test(email)
+  }
+
+  submitForm = () => {
+    const name = this.state.name
+    const email = this.state.email
+    const description = this.state.description
+
+    const isBlank = this.validateRequiredInput(name, email, description)
+    const isValidEmail = this.validateEmailFormat(email)
+
+    if (isBlank) {
+      alert('必須有力欄が空白です')
+      return false
+    } else if (!isValidEmail) {
+      alert('メールアドレスの書式が異なります')
+      return false
+    } else {
 
 
 
-}
+      const payload = {
+        text: '■お問い合わせがありました\n' +
+          'お名前:' + name + '\n' +
+          'Email:' + email + '\n' +
+          'お問い合わせ内容:' + '\n' + description
+      }
+
+      const url = 'https://hooks.slack.com/services/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'
+
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(payload)
+      }).then(() => {
+        alert('送信が完了しました。追ってご連絡します!')
+        this.setState({
+          name: "",
+          email: "",
+          description: ""
+        })
+        return this.props.handleClose()
+      })
+
+    }
 
 
-render() {
-  return (
-    <Dialog
-    open={this.props.open}
-    onClose={this.props.handleClose}
-    aria-labelledby="alert-dialog-title"
-    aria-describedby="alert-dialog-description"
-  >
-    <DialogTitle id="alert-dialog-title">お問合せフォーム</DialogTitle>
-    <DialogContent>
-      <DialogContentText>
-        <TextInput 
-          label={"お名前(必須)"} multiline={false} rows={1} value={this.state.name}
-          type={"text"} onChange={this.inputName}
-        />
-        <TextInput 
-          label={"メールアドレス(必須)"} multiline={false} rows={1} value={this.state.email}
-          type={"email"} onChange={this.inputEmail}
-        />
-        <TextInput 
-          label={"お問い合わせ内容(必須)"} multiline={true} rows={5} value={this.state.description}
-          type={"text"} onChange={this.inputDescription}
-        />
-      </DialogContentText>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={this.props.handleClose} color="primary">
-        キャンセル
+
+
+
+  }
+
+
+  render() {
+    return (
+      <Dialog
+        open={this.props.open}
+        onClose={this.props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">お問合せフォーム</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <TextInput
+              label={"お名前(必須)"} multiline={false} rows={1} value={this.state.name}
+              type={"text"} onChange={this.inputName}
+            />
+            <TextInput
+              label={"メールアドレス(必須)"} multiline={false} rows={1} value={this.state.email}
+              type={"email"} onChange={this.inputEmail}
+            />
+            <TextInput
+              label={"お問い合わせ内容(必須)"} multiline={true} rows={5} value={this.state.description}
+              type={"text"} onChange={this.inputDescription}
+            />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.handleClose} color="primary">
+            キャンセル
       </Button>
-      <Button onClick={this.submitForm} color="primary" autoFocus>
-        送信する
+          <Button onClick={this.submitForm} color="primary" autoFocus>
+            送信する
       </Button>
-    </DialogActions>
-  </Dialog>
-  )
-}
+        </DialogActions>
+      </Dialog>
+    )
+  }
 
 }

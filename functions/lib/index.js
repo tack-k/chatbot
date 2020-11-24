87fa68041fc9e4e3007eac599ba7a7daffdb7f4e
tack-k <tyool9c@gmail.com>
2020-11-24 11:04:22 +0900

    firebase init設定

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,27 @@
 "use strict";
-// import * as functions from 'firebase-functions';
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addDataset = void 0;
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+const sendResponse = (response, statusCode, body) => {
+    response.send({
+        statusCode,
+        body: JSON.stringify(body),
+    });
+};
+exports.addDataset = functions.https.onRequest(async (req, res) => {
+    if (req.method !== 'POST') {
+        sendResponse(res, 405, { error: 'Invalid Request!' });
+    }
+    else {
+        const dataset = req.body;
+        for (const key of Object.keys(dataset)) {
+            const data = dataset[key];
+            await db.collection('questions').doc(key).set(data);
+        }
+        sendResponse(res, 200, { message: 'Successfully added dataset!' });
+    }
+});
 //# sourceMappingURL=index.js.map
